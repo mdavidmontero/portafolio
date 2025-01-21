2ff4a86e654fa999af64f2d26a618d7113f87967
Mateo Rodriguez <mdavidmontero6@gmail.com>
2025-01-20 23:47:34 -0500

    Correciones diseño

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -1,18 +1,19 @@
 import AvatarServices from "@/components/avatar-services";
 import CircleImage from "@/components/circle-image";
+import ContainerPage from "@/components/container-page";
 import SliderServices from "@/components/slider-services";
 import TransitionPage from "@/components/transition-page";
 
 const ServicesPage = () => {
   return (
-    <>
+    <ContainerPage>
       <TransitionPage />
       <CircleImage />
       <AvatarServices />
-      <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
+      <div className="grid  justify-center items-center max-w-5xl gap-6 mx-auto md:grid-cols-2">
         <div className="max-w-[450px]">
           <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
-            Mis <span className="font-bold text-secondary"> servicios.</span>
+            Mis <span className="font-bold text-secondary"> Servicios</span>
           </h1>
           <p className="mb-3 text-xl text-gray-300">
             Ofrezco servicios de desarrollo web frontend para la creación de
@@ -26,12 +27,11 @@ const ServicesPage = () => {
           </button>
         </div>
 
-        {/* SLIDER */}
         <div>
           <SliderServices />
         </div>
       </div>
-    </>
+    </ContainerPage>
   );
 };
 
--- components/slider-services.tsx
+++ components/slider-services.tsx
@@ -1,7 +1,7 @@
 "use client";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
+import "swiper/css";
 
 import { serviceData } from "@/data";
 
@@ -31,7 +31,7 @@ const SliderServices = () => {
     >
       {serviceData.map((item, index) => (
         <SwiperSlide key={index}>
-          <div className="flex px-6 py-8 h-auto md:h-[290px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col gap-x-6 sm:gap-x-0 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-secondary border-2">
+          <div className="flex px-6 py-8 h-auto md:h-[320px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col gap-x-6 sm:gap-x-0 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-secondary border-2 justify-center items-center  ">
             <div className="mb-4 text-4xl text-secondary">{item.icon}</div>
             <div>
               <h3 className="mb-4 text-lg">{item.title}</h3>
